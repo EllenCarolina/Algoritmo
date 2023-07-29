6679015cb021508fae56ffeb07d303524a1ac97c
Ellen Carolina <nellenasuna@gmail.com>
2023-07-29 20:24:06 -0300

    first commit

---
+++ Conte1a10.pas
@@ -0,0 +1,12 @@
+uses 'Conte1a10';
+Var
+    count, n: Integer;
+begin
+ 	count:= 1;
+	write('Digite o número que deseja multiplicar: ')
+	read(n)
+	repeat
+		writeln(count);
+		count:= n * (count + 1);
+  until (count > 10)
+end.
